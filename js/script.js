c8f7fe489b4eb6ee5300cc86f25a2e93e4c691ea
minnja6 <jasmine.minniweather@verizon.com>
2019-06-22 14:22:51 -0500

    still working
    still working

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 //creating my two global variables that will be 
 
-const studentList = document.querySelectorAll('body ul li');
-const numberOfItemsOnPage = 10;
-console.log(studentList);
+const list = document.getElementsByClassName("student-item");
+const itemsPerPage = 10;
+console.log(list);
 
 
 //creating a function with the variable name "showPage" with 2 parameters that hides all of the students except for 10
-const showPage = (studentList, page) => {
-const startIndex = (page * numberOfItemsOnPage) - numberOfItemsOnPage;
-const endIndex = page * numberOfItemsOnPage;
+const showPage = (list, page) => {
+const startIndex = page * itemsPerPage - itemsPerPage;
+const endIndex = page * itemsPerPage -1;
 
-for ( i=0; i < studentList.length; i +=1) {
+for ( i=0; i < list.length; i ++) {
    if (i >= startIndex && i < endIndex) {
-      studentList[i].style.display = 'block'
+      list[i].style.display = 'block'
    } else {
-      studentList[i].style.display = 'none'
+      list[i].style.display = 'none'
    }  
 } 
-}
+};
 
-showPage(studentList, 1);
+showPage(list, 1);
 
-const getNumberOfPages =(itemsPerPage, list)=>{
+const numPages =(itemsPerPage, list)=>{
 return Math.ceil(list.length/itemsPerPage);
 };
 
@@ -29,40 +29,34 @@ return Math.ceil(list.length/itemsPerPage);
 // Creating the "appendPageLinks function" to generate, append, and add 
 // functionality to the pagination buttons.
 
-const appendPageLinks = (studentList) => { 
+const appendPageLinks = (list) => { 
 
-let div = document.createElement('div');
-let ul = document.createElement('ul');
-let page = document.querySelector('.page');
-div.setAttribute('class', 'pagination');
+let paginationDiv = document.createElement('div');
+let paginationLinks = document.createElement('ul');
+let pageDiv = document.querySelector('.page');
+paginationDiv.setAttribute('class', 'pagination');
 
-page.appendChild(div); 
-div.appendChild(ul); 
-
-let numberOfPages = getNumberOfPages(numberOfItemsOnPage, studentList);
+pageDiv.appendChild(paginationDiv); 
+paginationDiv.appendChild(paginationLinks); 
 
 // creating a list item with an anchor
 // appending the list item to the list
-for (i=1; i <= numberOfPages; i++){ 
+for (i=1; i < numPages + 1; i++){ 
 let li = document.createElement('li');
 let a = document.createElement('a');
-ul.appendChild(li);
+paginationLinks.appendChild(li);
 li.appendChild(a);
 a.setAttribute('href', '#');
-a.innerHTML = i;
+a.textContent = i;
 
+paginationLinks.firstElementChild.firstElementChild.className = "active";
 //creating the event listener to listen for a "click" to change the page/removing the active class in my for loop, adding the active class back outside of the for loop
-a.addEventListener('click', (e) => {
-   const aTag = document.querySelectorAll('a');
-   const page = event.target.textContent;
-   for (let j=0; j < aTag.length; j += 1) { 
-      const pageLink = aTag[j];
-      const link = (pageLink.textContent);
-      if (linkNumber === page){
-         pageLink.setAttribute('class' , 'active');
-      } else {
-      pageLink.removeAttribute('class');
-);}
-} 
-showPage(studentList, 1);
-appendPageLinks(studentList);
+a.addEventListener('click', (event) => {
+page = parseInt(e.target.textContent);
+showPage(list, page);
+let a = document.querySelectorAll('a');
+for(let i = 0; i < a.length; i++) {
+   a[i].className = "";
+}
+event.target.className = "active";
+});

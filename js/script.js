dca260f266eb1b13e5a94abb41092adacc6193f7
minnja6 <jasmine.minniweather@verizon.com>
2019-06-24 11:46:51 -0500

    just need to organize and add comments
    organzing and adding more comments next

--- js/script.js
+++ js/script.js
@@ -13,9 +13,9 @@ console.log(studentList);
 //looping over the list parameter 
    for (let i = 0; i < studentList.length; i ++) {
       if ( i >= startIndex && i < endIndex) {
-          studentList[i].style.display = 'block';
+          studentList[i].style.display = 'block'; //showing 10 students on each page
       } else {
-         studentList[i].style.display = 'none';
+         studentList[i].style.display = 'none'; //hiding all other students 
    }  
   } 
  };
@@ -24,18 +24,18 @@ showPage(studentList, 1);
 // Creating the "appendPageLinks function" to generate, append, and add 
 // functionality to the pagination buttons.
 
-         const appendPageLinks = (studentList) => { 
+      const appendPageLinks = (studentList) => { 
       //determining how many pages are needed      
          let getNumberOfPages = Math.ceil(studentList.length / itemsPerPage);
       //creating a div element with and giving it the class name of pagination
          let div = document.createElement('div');
          div.className = "pagination";
       //creating the ul element and append the page to .page   
-      let page = document.querySelector('.page');
-      page.appendChild(div);
+         let page = document.querySelector('.page');
+         page.appendChild(div);
       //adding a ul to the "pagination" div to store the pagination links 
-      let ul = document.createElement('ul');
-      div.appendChild(ul); 
+         let ul = document.createElement('ul');
+         div.appendChild(ul); 
 
       // creating a list item with an anchor
       // appending the list item to the list
@@ -43,22 +43,24 @@ showPage(studentList, 1);
          let li = document.createElement('li');
          let a = document.createElement('a');
          ul.appendChild(li);
-      li.appendChild(a);
-      a.setAttribute('href', '#');
-      a.textContent = j;
-
-      if (j === 0) {
-         a.className = 'active';
-      }
+         li.appendChild(a);
+         a.setAttribute('href', '#');
+         a.textContent = j;
+     
+         if (j === 1) {
+            a.className = 'active';
+         }
       //creating the event listener to listen for a "click" to change the page/removing the active class in my for loop, adding the active class back outside of the for loop
       a.addEventListener('click', (e) => {
-      let a = document.querySelectorAll('a');
-      for(let k = 0; k < a.length; k++) {
-         a.className = '';
-      }
-      let active = e.target.textContent;
+         let active = e.target.textContent;
+         let a = document.querySelectorAll('a');
+         showPage(studentList, active);
+            for(let k = 0; k < a.length; k++) {
+               a[k].classList.remove("active");
+            }
+         event.target.className = 'active';
      
-      showPage(studentList, active);
+      
       });
       }
       };
